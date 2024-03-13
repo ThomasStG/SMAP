da9c2458299398eeab9c5caaee00f869a2ee8745
Thomas Gallaher <ThomasSGallaher@gmail.com>
2024-03-13 13:36:07 -0400

    final commit for testingtmp

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ import os
 from server import startApp
 
 if len(sys.argv) > 1:
-    if sys.argv[1] == "g":
+    if sys.argv[1] == "i" or sys.argv[1] == "install" or sys.argv[1] == "get":
         subprocess.run(["pip", "install", "-r", "./static/documentation/requirements.txt"], check=True)
     else:
         for item in os.listdir('.'):
             if item.endswith("_test.py"):
-                subprocess.run(["python3", item], check=True)
-        
+                subprocess.run(["python3", item], check=True)       
 else:
     startApp()
-# LF (\n) (EOF)
