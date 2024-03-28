b42e3d556fbd37e4032c3cf12d789ca56d35b7b5
Thomas Gallaher <ThomasSGallaher@gmail.com>
2024-03-28 13:38:28 -0400

    try again

--- app.py
+++ app.py
@@ -6,10 +6,21 @@ from server import startApp
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "i" or sys.argv[1] == "install" or sys.argv[1] == "get":
-        subprocess.run(["pip", "install", "-r", "./static/documentation/requirements.txt"], check=True)
+
+        # Get the path to the Python executable
+        python_executable = sys.executable
+
+        # Calculate the path to pip based on the Python executable path
+        pip_path = os.path.join(os.path.dirname(python_executable), "pip")
+
+        subprocess.run([
+            pip_path, "install", "-r",
+            "./static/documentation/requirements.txt"
+        ],
+                       check=True)
     else:
         for item in os.listdir('.'):
             if item.endswith("_test.py"):
-                subprocess.run(["python3", item], check=True)       
+                subprocess.run(["python3", item], check=True)
 else:
     startApp()
